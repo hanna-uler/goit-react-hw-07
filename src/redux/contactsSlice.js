0758eccb7c483792cb585b08156ebf651bb24479
hanna-uler <hannauler@gmail.com>
2025-06-06 12:27:16 -0400

    change contactsSlice reducers

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -9,18 +9,24 @@ const contactsSlice = createSlice({
             { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
             { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
         ],
-        loading: false,
+        isLoading: false,
         error: null,
     },
     reducers: {
-        addContact: (state, action) => {
-            state.items.push(action.payload);
+        fetchInProgress: (state) => {
+            state.isLoading = true;
         },
-        deleteContact: (state, action) => {
-            state.items = state.items.filter((item) => item.id !== action.payload)
-        }
-    }
+        fetchSuccess: (state, action) => {
+            state.isLoading = false;
+            state.error = null;
+            state.items = action.payload;
+         },
+        fetchError: (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        },
+      },
     })
 
 export default contactsSlice.reducer;
-export const { addContact, deleteContact } = contactsSlice.actions;
+export const { fetchInProgress, fetchSuccess, fetchError } = contactsSlice.actions;
